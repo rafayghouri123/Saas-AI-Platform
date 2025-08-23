9472913897960dca574a91aee15a9a2d90d41c02
rafayghouri123 <rafayghouri50@gmail.com>
2025-08-23 23:34:32 +0500

    widget basic work

--- apps/web/app/modules/dashboard/ui/components/dashboard-sidebar.tsx
+++ apps/web/app/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -3,6 +3,7 @@
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
 import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarRail } from "@workspace/ui/components/sidebar"
 import { Tooltip } from "@workspace/ui/components/tooltip"
+import { cn } from "@workspace/ui/lib/utils"
 import { CreditCardIcon, InboxIcon, LayoutDashboardIcon, Library, LibraryBigIcon, Mic, PaletteIcon } from "lucide-react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
@@ -11,164 +12,167 @@ import { usePathname } from "next/navigation"
 
 
 
-export const DashboardSidebar = ()=>{
-    const customerSupportItems=[
+export const DashboardSidebar = () => {
+    const customerSupportItems = [
         {
-            title:"Coversations",
-            url:"/conversations",
-            icon:InboxIcon
+            title: "Coversations",
+            url: "/conversations",
+            icon: InboxIcon
         },
 
         {
-            title:"Knowledge Base",
-            url:"/files",
-            icon:LibraryBigIcon
+            title: "Knowledge Base",
+            url: "/files",
+            icon: LibraryBigIcon
         },
     ]
 
-    const ConfigurationItems=[
+    const ConfigurationItems = [
         {
-            title:"Integrations",
-            url:"/integrations",
-            icon:LayoutDashboardIcon
+            title: "Integrations",
+            url: "/integrations",
+            icon: LayoutDashboardIcon
         },
 
         {
-            title:"Widget Customizations",
-            url:"/customizations",
-            icon:PaletteIcon
+            title: "Widget Customizations",
+            url: "/customizations",
+            icon: PaletteIcon
         },
         {
-            title:"Voice Assistance",
-            url:"/plugins/vapi",
-            icon:Mic
+            title: "Voice Assistance",
+            url: "/plugins/vapi",
+            icon: Mic
         },
     ]
 
-      const AccountItems=[
+    const AccountItems = [
         {
-            title:"Plans & Billings",
-            url:"/billings",
-            icon:CreditCardIcon
+            title: "Plans & Billings",
+            url: "/billings",
+            icon: CreditCardIcon
         },
     ]
 
-    
+
 
     const path = usePathname()
 
-    const isActive=(url:string)=>{
-        if(url ==="/"){
-            return path ==="/"
+    const isActive = (url: string) => {
+        if (url === "/") {
+            return path === "/"
         }
         return path.startsWith(url)
     }
 
-    return(
+    return (
         <Sidebar className="group" collapsible="icon">
-                <SidebarHeader>
-                    <SidebarMenu>
-                        <SidebarMenuItem>
-                            <SidebarMenuButton asChild size="lg">
-                              <OrganizationSwitcher  hidePersonal 
-                                                    skipInvitationScreen
-                                                    appearance={{
-                                                        elements:{
-                                                            rootBox:"w-full! h-8!",
-                                                            avatarBox:"size-4! rounded-sm!",
-                                                            organizationSwitcherTrigger:"w-full! justify-start! group-data-[collapsible=icon]:size-8! group-data-[collapsible=icon]:p-2!",
-                                                            organizationPreview:"group-data-[collapsible=icon]:justify-center! gap-2!",
-                                                            organizationPreviewTextContainer:"group-data-[collapsible=icon]:hidden! text-xs font-medium! text-sidebar-foreground!",
-                                                            organizationSwitcherTriggerIcon:"group-data-[collapsible=icon]:hidden! ml-auto!  text-sidebar-foreground!"
-                                                        }
-                                                    }}/>
-                            </SidebarMenuButton>
-                        </SidebarMenuItem>
-                    </SidebarMenu>
-                </SidebarHeader>
-                <SidebarContent> 
-                    {/*Customer support*/}
-                    <SidebarGroup>
-                        <SidebarGroupLabel>Customers support</SidebarGroupLabel>
-                        <SidebarGroupContent>
-                            <SidebarMenu>
-                                {customerSupportItems.map((items)=>(
-                                    <SidebarMenuItem key={items.title}>
-                                        <SidebarMenuButton
-                                           asChild
-                                           isActive={isActive(items.url)}
-                                           tooltip={items.title}>
-                                            <Link href={items.url}>
-                                                <items.icon className="size-4"></items.icon>
-                                                <span>{items.title}</span>
-                                            </Link>
-                                        </SidebarMenuButton>
-                                    </SidebarMenuItem>
-                                ))}
-                            </SidebarMenu>
-                        </SidebarGroupContent>
-                    </SidebarGroup>
-                       {/*Configurations*/}
-                    <SidebarGroup>
-                        <SidebarGroupLabel>Configurations</SidebarGroupLabel>
-                        <SidebarGroupContent>
-                            <SidebarMenu>
-                                {ConfigurationItems.map((items)=>(
-                                    <SidebarMenuItem key={items.title}>
-                                        <SidebarMenuButton
-                                           asChild
-                                           isActive={isActive(items.url)}
-                                           tooltip={items.title}>
-                                            <Link href={items.url}>
-                                                <items.icon className="size-4"></items.icon>
-                                                <span>{items.title}</span>
-                                            </Link>
-                                        </SidebarMenuButton>
-                                    </SidebarMenuItem>
-                                ))}
-                            </SidebarMenu>
-                        </SidebarGroupContent>
-                    </SidebarGroup>
-                     {/*Billings*/}
-                    <SidebarGroup>
-                        <SidebarGroupLabel>Account</SidebarGroupLabel>
-                        <SidebarGroupContent>
-                            <SidebarMenu>
-                                {AccountItems.map((items)=>(
-                                    <SidebarMenuItem key={items.title}>
-                                        <SidebarMenuButton
-                                           asChild
-                                           isActive={isActive(items.url)}
-                                           tooltip={items.title}>
-                                            <Link href={items.url}>
-                                                <items.icon className="size-4"></items.icon>
-                                                <span>{items.title}</span>
-                                            </Link>
-                                        </SidebarMenuButton>
-                                    </SidebarMenuItem>
-                                ))}
-                            </SidebarMenu>
-                        </SidebarGroupContent>
-                    </SidebarGroup>
-                </SidebarContent>
-                <SidebarFooter>
-                    <SidebarMenu>
-                        <SidebarMenuItem>
-                            <UserButton
-                              showName
-                              appearance={{
-                                elements:{
-                                    rootBox:"w-full! h-8!",
-                                    userButtonTrigger:"w-full! p-2! hover:bg-sidebar-accent! hover:text-sidebar-accent-foreground! group-data-[collapsible=icon]:size-8! group-data-[collapsible=icon]:p-2!",
-                                    userButtonBox:"w-full! flex-row-reverse! justify-end! group-data-[collapsible=icon]:justify-center! gap-2! text-sidebar-accent-foreground!",
-                                    userButtonOuterIdentifier:"pl-0! group-data-[collapsible=icon]:hidden!",
-                                    avatarBox:"size-4!"
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton asChild size="lg">
+                            <OrganizationSwitcher hidePersonal
+                                skipInvitationScreen
+                                appearance={{
+                                    elements: {
+                                        rootBox: "w-full! h-8!",
+                                        avatarBox: "size-4! rounded-sm!",
+                                        organizationSwitcherTrigger: "w-full! justify-start! group-data-[collapsible=icon]:size-8! group-data-[collapsible=icon]:p-2!",
+                                        organizationPreview: "group-data-[collapsible=icon]:justify-center! gap-2!",
+                                        organizationPreviewTextContainer: "group-data-[collapsible=icon]:hidden! text-xs font-medium! text-sidebar-foreground!",
+                                        organizationSwitcherTriggerIcon: "group-data-[collapsible=icon]:hidden! ml-auto!  text-sidebar-foreground!"
+                                    }
+                                }} />
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent>
+                {/*Customer support*/}
+                <SidebarGroup>
+                    <SidebarGroupLabel>Customers support</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {customerSupportItems.map((items) => (
+                                <SidebarMenuItem key={items.title}>
+                                    <SidebarMenuButton
+                                        asChild
+                                        isActive={isActive(items.url)}
+                                        className={cn(isActive(items.url)&&"bg-gradient-to-b from-sidebar-primary to-[#0b63f3]! text-sidebar-primary-foreground! hover:to-[#0b63f3]/90! ")}
+                                        tooltip={items.title}>
+                                        <Link href={items.url}>
+                                            <items.icon className="size-4"></items.icon>
+                                            <span>{items.title}</span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+                {/*Configurations*/}
+                <SidebarGroup>
+                    <SidebarGroupLabel>Configurations</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {ConfigurationItems.map((items) => (
+                                <SidebarMenuItem key={items.title}>
+                                    <SidebarMenuButton
+                                        asChild
+                                        isActive={isActive(items.url)}
+                                         className={cn(isActive(items.url)&&"bg-gradient-to-b from-sidebar-primary to-[#0b63f3]! text-sidebar-primary-foreground! hover:to-[#0b63f3]/90! ")}
+                                        tooltip={items.title}>
+                                        <Link href={items.url}>
+                                            <items.icon className="size-4"></items.icon>
+                                            <span>{items.title}</span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+                {/*Billings*/}
+                <SidebarGroup>
+                    <SidebarGroupLabel>Account</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {AccountItems.map((items) => (
+                                <SidebarMenuItem key={items.title}>
+                                    <SidebarMenuButton
+                                        asChild
+                                        isActive={isActive(items.url)}
+                                         className={cn(isActive(items.url)&&"bg-gradient-to-b from-sidebar-primary to-[#0b63f3]! text-sidebar-primary-foreground! hover:to-[#0b63f3]/90! ")}
+                                        tooltip={items.title}>
+                                        <Link href={items.url}>
+                                            <items.icon className="size-4"></items.icon>
+                                            <span>{items.title}</span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <UserButton
+                            showName
+                            appearance={{
+                                elements: {
+                                    rootBox: "w-full! h-8!",
+                                    userButtonTrigger: "w-full! p-2! hover:bg-sidebar-accent! hover:text-sidebar-accent-foreground! group-data-[collapsible=icon]:size-8! group-data-[collapsible=icon]:p-2!",
+                                    userButtonBox: "w-full! flex-row-reverse! justify-end! group-data-[collapsible=icon]:justify-center! gap-2! text-sidebar-accent-foreground!",
+                                    userButtonOuterIdentifier: "pl-0! group-data-[collapsible=icon]:hidden!",
+                                    avatarBox: "size-4!"
                                 }
-                              }}/>
-                        </SidebarMenuItem>
-                    </SidebarMenu>
-                </SidebarFooter>
-                <SidebarRail/>
+                            }} />
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarFooter>
+            <SidebarRail />
         </Sidebar>
     )
 }
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,29 +1,21 @@
+
 "use client"
-import { Button } from "@workspace/ui/components/button"
-import { useVapi } from "@/modules/widget/hooks/use-vapi"
+import { WidgetView } from '@/modules/widget/ui/views/widget-view'
+import  { use } from 'react'
 
-export default function Page() {
+ interface Props {
+    searchParams:Promise<{
+    organizationId :string
+ }>
+ }
 
-   const {
-        isSpeaking,
-        isConnected,
-        isConnecting,
-        transcript,
-        startCall,
-        endCall}= useVapi()
+const page = ({searchParams}:Props) => {
+  const {organizationId} = use(searchParams)
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World/ Widget</h1>
-      
-        <Button size="sm" onClick={()=>{startCall()}} >Start call</Button>
-         <Button size="sm" onClick={()=>{endCall()}} variant="destructive" >End call</Button>
-     
-      </div>
-      <p>isConnected:{`${isConnected}`}</p>
-      <p>isConnecting:{`${isConnecting}`}</p>
-      <p>isSpeaking:`${isSpeaking}`</p>
-      <p>{JSON.stringify(transcript,null,2)}</p>
-    </div>
+    
+    <WidgetView organizationId={organizationId}/>
+ 
   )
 }
+
+export default page
